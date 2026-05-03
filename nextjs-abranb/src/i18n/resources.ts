f6e599d545744234c21ffa692b6ba534704d5652
Lian Klein <lian@loadsmart.com>
2026-05-03 12:27:00 -0300

    Add page Nossa Historia - section O que somos

--- nextjs-abranb/src/i18n/resources.ts
+++ nextjs-abranb/src/i18n/resources.ts
@@ -4,14 +4,18 @@ import ptBRCommon from "./locales/pt-BR/common.json";
 import enCommon from "./locales/en/common.json";
 import ptBRHome from "./locales/pt-BR/home.json";
 import enHome from "./locales/en/home.json";
+import ptBRHistoria from "./locales/pt-BR/historia.json";
+import enHistoria from "./locales/en/historia.json";
 
 export const resources = {
   "pt-BR": {
     common: ptBRCommon,
     home: ptBRHome,
+    historia: ptBRHistoria,
   },
   en: {
     common: enCommon,
     home: enHome,
+    historia: enHistoria,
   },
 } satisfies Resource;
