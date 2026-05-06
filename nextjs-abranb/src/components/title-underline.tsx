5a65703f80042556fdce7a0528fadc4981dbcb56
Sabrina Souza <sabrinasouza.dev@gmail.com>
2026-05-05 22:33:07 -0300

    feat: Add section Entenda a não-binariedade

---
+++ nextjs-abranb/src/components/title-underline.tsx
@@ -0,0 +1,16 @@
+type TitleUnderlineProps = {
+    title: string;
+    color?: string;
+    position: "start" | "center"
+}
+
+export default function TitleUnderline({ title, color = 'title-decoration', position }: TitleUnderlineProps) {
+    return (
+        <div className={`flex flex-col items-center max-lg:text-center lg:items-${position} justify-${position}`}>
+            <h2 className={`text-display ${color === 'gold' ? 'text-gold' : 'text-title-decoration'} tracking-wide`}>
+                {title}
+            </h2>
+            <div className={`mt-2 h-0.5 w-1/2 lg:w-3/4 ${color === 'gold' ? 'bg-gold' : 'bg-title-decoration'} rounded-full`}></div>
+        </div>
+    )
+}
