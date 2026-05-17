03de80e5156133ff9e695b596653a11b7fd0f780
Sabrina Souza <sabrinasouza.dev@gmail.com>
2026-05-17 14:22:24 -0300

    feat: add seção sintese de pautas

--- nextjs-abranb/src/components/home-section-placeholders.tsx
+++ nextjs-abranb/src/components/home-section-placeholders.tsx
@@ -6,6 +6,7 @@ import EntendaNB from "./componentsHome/sections/entenda-nb";
 import OQueFizemos from "./componentsHome/sections/OQueFizemos";
 import NossaHistoria from "./componentsHome/sections/nossa-historia";
 import Coletives from "./componentsHome/sections/coletives";
+import SinteseDePautas from "./componentsHome/sections/sintese-pautas";
 
 // TODO: Montar as seções da página home
 export function HomeSectionPlaceholders() {
@@ -13,7 +14,8 @@ export function HomeSectionPlaceholders() {
   "o-que-e-nao-binario": EntendaNB,
   "noticias-abranb": OQueFizemos,
   "nossa-historia": NossaHistoria,
-  "nossas-conexoes": Coletives
+  "nossas-conexoes": Coletives,
+  "sintese-pautas": SinteseDePautas
 };
 
   return (
