dcd9abe3acdf7539accc2788db143223e28c3bca
Lian Klein <lian@loadsmart.com>
2026-07-17 21:22:00 -0300

    Remove old PostList from bottom of Home page

--- nextjs-abranb/src/app/page.tsx
+++ nextjs-abranb/src/app/page.tsx
@@ -1,28 +1,15 @@
-import { type SanityDocument } from "next-sanity";
-
 import { HomeScrollToHash } from "@/components/home-scroll-to-hash";
-import { HomeSectionPlaceholders } from "@/components/home-section-placeholders";
-import { PostsList } from "@/components/posts-list";
-import { client } from "@/sanity/client";
+import { HomePageSections } from "@/components/home-page-sections";
 import Hero from "@/components/componentsHome/sections/hero";
 
-const POSTS_QUERY = `*[
-  _type == "post"
-  && defined(slug.current)
-]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt}`;
-
-const options = { next: { revalidate: 30 } };
-
 export default async function IndexPage() {
-  const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
 
   return (
     <>
       < HomeScrollToHash />
       <main className="container min-h-screen max-w-full">
         <Hero />
-        <HomeSectionPlaceholders />
-        <PostsList posts={posts} />
+        <HomePageSections />
       </main>
     </>
   );
